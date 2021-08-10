220df41db7432e2b9bfc4b3bef2d180664cc42a4
Wegmann Peter <peter.wegmann@helmholtz-berlin.de>
2021-08-10 17:41:30 +0200

    added init function

--- main.c
+++ main.c
@@ -1,18 +1,40 @@
 /*
- * ilm-module.c
- *
- * Created: 10.08.2021 11:34:03
- * Author : qfj
- */ 
+* ilm-module.c
+*
+* Created: 10.08.2021 11:34:03
+* Author : qfj
+*/
 
 #include <avr/io.h>
+#include <stdio.h>
+
+#include "main.h"
+#include "usart.h"
+#include "xbee.h"
+
+
+void init(void){
+	usart_init(39);  //USART0 init with 9600 baud
+	usart1_init(39); //USART1 init with 9600 baud
+	xbee_init(NULL,NULL,0);
+	
+	//=========================================================================
+	// Enable global interrupts
+	//=========================================================================
+	sei();
+
+	
+	
+}
 
 
 int main(void)
 {
-    /* Replace with your application code */
-    while (1) 
-    {
-    }
+	init();
+	
+	/* Replace with your application code */
+	while (1)
+	{
+	}
 }
 
---
+++ main.h
@@ -0,0 +1,16 @@
+/*
+ * main.h
+ *
+ * Created: 10.08.2021 16:47:29
+ *  Author: qfj
+ */ 
+
+
+#ifndef MAIN_H_
+#define MAIN_H_
+
+
+
+
+
+#endif /* MAIN_H_ */
