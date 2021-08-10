bec148a2cc9c275bb1f5a957a6e30cc9cc7ea696
Wegmann Peter <peter.wegmann@helmholtz-berlin.de>
2021-08-10 11:36:35 +0200

    added submodule avrlib

---
+++ main.c
@@ -0,0 +1,18 @@
+/*
+ * ilm-module.c
+ *
+ * Created: 10.08.2021 11:34:03
+ * Author : qfj
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
